ad9783e8a4601d9b5bb43e7b4f31f86c3eead29e
Filipe Costa <filipebarcos@gmail.com>
2013-07-30 23:16:41 -0300

    more env vars

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,8 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export RBENV_HOME=$HOME/.rbenv
+export GOHOME=/usr/local/go
+export GOPATH=$HOME/workspace/go
+export JSC_HOME=/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
+
+export PATH="./bin:$RBENV_HOME/shims:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.sfs:$ZSH/bin:$JSC_HOME:$GOHOME/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
