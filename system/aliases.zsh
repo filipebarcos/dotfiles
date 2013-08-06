1172662314c36995d54f2258fc1382933e100ebb
Filipe Costa <filipebarcos@gmail.com>
2013-08-05 23:37:39 -0300

    new alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -26,3 +26,6 @@ alias extip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias wifiip="ipconfig getifaddr en1"
 alias cableip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
+
+#biggest files
+alias big-files="du -a / | sort -n -r | head -n 10"
