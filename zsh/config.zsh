8e9c3fd867514b85366988dc97b2c104b2dd8b1b
Filipe Costa <filipebarcos@gmail.com>
2013-08-09 01:02:26 -0300

    fixing bindkeys

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -44,8 +44,5 @@ bindkey -v
 bindkey '^R' history-incremental-search-backward
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
