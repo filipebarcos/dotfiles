c47c6cc84ed47e36f7b6a8ae97fb692f5a944c56
Filipe Costa <filipebarcos@gmail.com>
2015-05-26 01:46:39 -0500

    removed cabal shit

--- system/path.zsh
+++ system/path.zsh
@@ -4,8 +4,7 @@ export GOPATH=$HOME/workspace/go
 export JSC_HOME=/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
 export ADT_HOME=/Applications/Android\ Studio.app/
 export ADT_PLATFORM_TOOLS=$ADT_HOME/sdk/platform-tools
-export CABAL_HOME=$HOME/.cabal
 
-export PATH="$CABAL_HOME/bin:./bin:$RBENV_HOME/shims:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.sfs:$ZSH/bin:$JSC_HOME:$GOHOME/bin:$ADT_PLATFORM_TOOLS:$PATH"
+export PATH="./bin:$RBENV_HOME/shims:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.sfs:$ZSH/bin:$JSC_HOME:$GOHOME/bin:$ADT_PLATFORM_TOOLS:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
