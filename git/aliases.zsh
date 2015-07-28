2f229577e7a483f87819bc09a22cffe37ad75612
Filipe Costa <filipebarcos@gmail.com>
2015-07-28 17:48:59 -0300

    face punch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,6 +11,7 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 
+alias facepunch="git push -f"
 alias whatlast="git show HEAD"
 alias ci="git commit"
 alias st="git status"
