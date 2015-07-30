b95094774da059e03a1c3c7bf7838813231fdf08
Filipe Costa <filipebarcos@gmail.com>
2015-07-30 12:02:14 -0300

    even more aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,6 @@
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -11,12 +10,15 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 
+alias gcm="git commit -m"
 alias gwip="git commit -m 'wip'"
 alias gp="git rev-parse --abbrev-ref HEAD | git push -u origin $1"
 alias gforce="git push -f"
 alias whatlast="git show HEAD"
 alias ci="git commit"
 alias st="git status"
+alias master="git checkout master"
+alias back="git checkout -"
 
 # hub related aliases
 alias pr="hub pull-request"
