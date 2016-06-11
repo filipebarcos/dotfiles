51bf8523742f18ae3224d458e25de1b0163bc064
Filipe Costa <filipebarcos@gmail.com>
2016-06-11 14:34:58 -0300

    fixing console alias

--- elixir/aliases.zsh
+++ elixir/aliases.zsh
@@ -3,5 +3,5 @@ alias mmig="mix ecto.migrate"
 alias mreset="mix ecto.drop && mix ecto.create && mix ecto.migrate"
 alias mdget="mix deps.get"
 
-alias mc="iex -S mix"
+alias mc="iex -S mix phoenix.server"
 alias mps="mix phoenix.server"
