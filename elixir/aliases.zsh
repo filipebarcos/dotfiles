7040ba1150f823e31f278de59fb7193f2acec742
Filipe Costa <filipebarcos@gmail.com>
2016-06-11 14:07:01 -0300

    A few elixir/phoenix aliases

---
+++ elixir/aliases.zsh
@@ -0,0 +1,7 @@
+alias mt="mix test"
+alias mmig="mix ecto.migrate"
+alias mreset="mix ecto.drop && mix ecto.create && mix ecto.migrate"
+alias mdget="mix deps.get"
+
+alias mc="iex -S mix"
+alias mps="mix phoenix.server"
