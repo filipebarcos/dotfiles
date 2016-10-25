8229e97dd9d56a0d0a96f5970bfef4d4f8e82a2c
Filipe Costa <filipebarcos@gmail.com>
2016-10-25 09:28:34 -0300

    A few new aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,9 +2,12 @@ alias bil="bundle install"
 alias brake="bundle exec rake"
 alias be="bundle exec"
 
-alias sc='rails c'
+alias rc='rails c'
 
-alias migrate='rake db:migrate db:test:clone'
+alias migrate='bundle exec rake db:migrate db:test:clone'
+alias rrst='bundle exec rake db:reset'
+alias bspec='be rspec spec/'
+
 
 alias zpec='zeus rspec spec/'
 alias zake='zeus rake'
