6a25eebb08aa38f863636fe1accb923abef2b4d0
Filipe Costa <filipebarcos@gmail.com>
2017-12-08 11:58:00 -0500

    oh boy, i missed this

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,19 +1,10 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias ll="gls -lAhF --color"
-  alias l="gls -l --color"
-  alias la='gls -A --color'
-fi
 #Enable aliases to be sudo’ed
 alias sudo='sudo '
 alias workspace="cd ~/workspace"
 alias dot="cd ~/.dotfiles"
 alias src="cd ~/src"
 alias cls="clear"
+alias ll="ls -lash"
 
 #OSX
 # Show/hide hidden files in Finder
