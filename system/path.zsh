d41e89aecf9d541c92ec7de709b3b0fc26e10942
Filipe Costa <filipebarcos@gmail.com>
2018-02-15 15:20:31 -0500

    no boxen

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,2 @@
 export PATH="$HOME/bin:$ZSH/bin:$PATH"
 export PATH="$PATH:$HOME/.yarn/bin"
-
-if [ -f /opt/boxen/env.sh ]; then
-  source /opt/boxen/env.sh
-else
-  export PATH="$HOME/.rbenv/shims:/usr/local/heroku/bin:$PATH"
-fi
