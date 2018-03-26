991906fb67523e83c58661dea3cb0d939b8c87d1
Filipe Costa <filipebarcos@gmail.com>
2018-03-26 14:59:38 -0400

    improve

--- fzf/exports.zsh
+++ fzf/exports.zsh
@@ -1,2 +1,2 @@
-export FZF_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow --glob '!{.git,node_modules,log}/*'"
+export FZF_DEFAULT_COMMAND="git ls-tree -r --name-only || rg --files --hidden --follow --glob '!{.git,node_modules,log}/*'"
 # export FZF_DEFAULT_OPTS="--reverse --inline-info"
