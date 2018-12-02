d36644bdcbc25002e653dff2d1542a072b2ca039
Filipe Costa <filipebarcos@gmail.com>
2018-12-02 08:09:43 -0500

    peek

--- tmux/functions.zsh
+++ tmux/functions.zsh
@@ -3,5 +3,11 @@ tat() {
 }
 
 tnew() {
+  export CURRENT_TMUX_SESSION=$1
   tmux -u new-session -s $1
+}
+
+# Credits: https://github.com/lf94/peek-for-tmux
+peek() {
+  tmux split-window -p 33 "$EDITOR" "$@" || exit;
 }
