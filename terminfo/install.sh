0629564d9bc6a0ec71365db5ea22000d42763982
Filipe Costa <filipebarcos@gmail.com>
2019-02-20 23:01:20 -0500

    important to do this here

--- terminfo/install.sh
+++ terminfo/install.sh
@@ -9,4 +9,7 @@ sh -c "tic -o $HOME/.terminfo $DOTFILES_ROOT/terminfo/fixtures/tmux.terminfo"
 sh -c "tic -o $HOME/.terminfo $DOTFILES_ROOT/terminfo/fixtures/tmux-256color.terminfo"
 sh -c "tic -o $HOME/.terminfo $DOTFILES_ROOT/terminfo/fixtures/xterm-256color.terminfo"
 
+echo "Compiled. Now sourcing xterm-256color"
+[ -e ~/.tmux/fixtures/xterm-256color.terminfo ] && tic ~/.tmux/fixtures/xterm-256color.terminfo
+
 echo "Done ✅"
