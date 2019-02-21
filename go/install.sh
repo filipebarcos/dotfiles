b98815fe8326288c0f90142a838f4fe5d34afc04
Filipe Costa <filipebarcos@gmail.com>
2019-02-20 23:00:39 -0500

    fixing PATH

--- go/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-mkdir $HOME/gopath
-mkdir $HOME/gopath/bin
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,1 @@
-export PATH="$HOME/bin:$ZSH/bin:$PATH"
-export PATH="$PATH:$HOME/.yarn/bin"
-export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$HOME/bin:$ZSH/bin:$PATH:$HOME/.yarn/bin:$CARGO_PATH/bin:$GOPATH/bin"
