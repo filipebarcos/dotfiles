89f40f2751a4a8de09b51ab7330fcc07bfb9a61e
Filipe Costa <filipebarcos@gmail.com>
2019-04-10 10:43:50 -0400

    fzf

--- fzf/exports.zsh
+++ fzf/exports.zsh
@@ -1,3 +1,3 @@
 # export FZF_DEFAULT_COMMAND="git ls-tree -r --name-only || rg --files --hidden --follow --glob '!{.git,node_modules,log}/*'"
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!{.git,node_modules,log,rbi}/*'"
-# export FZF_DEFAULT_OPTS="--reverse --inline-info"
+export FZF_DEFAULT_OPTS="--inline-info"
