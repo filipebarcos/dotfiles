acb9e314ed0da49c8573d8a22477fa35ddf64adc
Filipe Costa <filipebarcos@gmail.com>
2020-07-14 10:37:48 -0300

    coc.vim and some other deps cleanup

--- vim/install.sh
+++ vim/install.sh
@@ -5,3 +5,13 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
+
+# Install extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+  echo '{"dependencies":{}}'> package.json
+fi
+
+vim -c 'CocInstall -sync coc-json coc-tsserver coc-go coc-sql'
