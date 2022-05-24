ee76e6cdec5ec7f8c54f2f5d8b3aae6d7d7c1afb
Filipe Costa <filipebarcos@stripe.com>
2022-05-23 22:04:28 -0700

    Fixing nvim installer

--- installers/nvim.sh
+++ installers/nvim.sh
@@ -2,8 +2,8 @@
 
 echo "  💽 Installing vim-plug"
 set -e
-# curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "  ✅ vim-plug installed"
 
